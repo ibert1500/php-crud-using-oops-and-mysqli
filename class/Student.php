0fc31d4f72ff69f40b295e7f528850314607cb8e
ibert1500 <ibert1500@gmail.com>
2025-10-04 08:20:28 +0300

    adddx

---
+++ class/Student.php
@@ -0,0 +1,67 @@
+<?php 
+require_once ("class/DBController.php");
+class Student
+{
+    private $db_handle;
+    
+    function __construct() {
+        $this->db_handle = new DBController();
+    }
+    
+    function addStudent($name, $roll_number, $dob, $class,$Major) {
+        $query = "INSERT INTO tbl_student (name,roll_number,dob,class,Major) VALUES (?, ?, ?, ?,?)";
+        $paramType = "sisss";
+        $paramValue = array(
+            $name,
+            $roll_number,
+            $dob,
+            $class,
+            $Major
+        );
+        
+        $insertId = $this->db_handle->insert($query, $paramType, $paramValue);
+        return $insertId;
+    }
+    
+    function editStudent($name, $roll_number, $dob, $class, $student_id,$Major) {
+        $query = "UPDATE tbl_student SET name = ?,roll_number = ?,dob = ?,class = ?,Major = ? WHERE id = ?";
+        $paramType = "sisssi";
+        $paramValue = array(
+            $name,
+            $roll_number,
+            $dob,
+            $class,
+            $Major,
+            $student_id
+        );
+        
+        $this->db_handle->update($query, $paramType, $paramValue);
+    }
+    
+    function deleteStudent($student_id) {
+        $query = "DELETE FROM tbl_student WHERE id = ?";
+        $paramType = "i";
+        $paramValue = array(
+            $student_id
+        );
+        $this->db_handle->update($query, $paramType, $paramValue);
+    }
+    
+    function getStudentById($student_id) {
+        $query = "SELECT * FROM tbl_student WHERE id = ?";
+        $paramType = "i";
+        $paramValue = array(
+            $student_id
+        );
+        
+        $result = $this->db_handle->runQuery($query, $paramType, $paramValue);
+        return $result;
+    }
+    
+    function getAllStudent() {
+        $sql = "SELECT * FROM tbl_student ORDER BY id";
+        $result = $this->db_handle->runBaseQuery($sql);
+        return $result;
+    }
+}
+?>
---
+++ web/student-add.php
@@ -0,0 +1,80 @@
+<?php require_once "web/header.php"; ?>
+
+<form name="frmAdd" method="post" action="" id="frmAdd"
+    onSubmit="return validate();">
+    <div id="mail-status"></div>
+    <div>
+        <label style="padding-top: 20px;">Name</label> <span
+            id="name-info" class="info"></span><br /> <input type="text"
+            name="name" id="name" class="demoInputBox">
+    </div>
+    <div>
+        <label>Roll Number</label> <span id="roll-number-info"
+            class="info"></span><br /> <input type="text"
+            name="roll_number" id="roll_number" class="demoInputBox">
+    </div>
+    <div>
+        <label>Date of Birth</label> <span id="dob-info" class="info"></span><br />
+        <input type="date" name="dob" id="dob" class="demoInputBox">
+    </div>
+    <div>
+        <label>Class</label> <span id="class-info" class="info"></span><br />
+        <input type="text" name="class" id="class" class="demoInputBox">
+    </div>
+
+    <div>
+        <label>Major</label> <span id="Major-info" class="info"></span><br />
+        <select name="Major" id="Major" class="demoInputBox" required>
+            <option value="">اختر التخصص</option>
+            <option value="ccc" <?php echo isset($Major) && $Major == 'ccc'? 'selected' : ''; ?>>ccc</option>
+            <option value="bbb" <?php echo isset($Major) && $Major == 'bbb'? 'selected' : ''; ?>>bbb</option>
+            <option value="ddd" <?php echo isset($Major) && $Major == 'ddd'? 'selected' : ''; ?>>ddd</option>
+            
+</select>
+    </div>
+    
+    <div> <input type="file" name="fileToUpload"/>  </div>
+    <div>
+
+        <input type ="submit" name="add" id="btnSubmit" value="Add" />
+    </div>
+    </div>
+</form>
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"
+    type="text/javascript"></script>
+<script>
+function validate() {
+    var valid = true;   
+    $(".demoInputBox").css('background-color','');
+    $(".info").html('');
+    
+    if(!$("#name").val()) {
+        $("#name-info").html("(required)");
+        $("#name").css('background-color','#FFFFDF');
+        valid = false;
+    }
+    if(!$("#roll_number").val()) {
+        $("#roll-number-info").html("(required)");
+        $("#roll_number").css('background-color','#FFFFDF');
+        valid = false;
+    }
+    if(!$("#dob").val()) {
+        $("#dob-info").html("(required)");
+        $("#dob").css('background-color','#FFFFDF');
+        valid = false;
+    }
+    if(!$("#class").val()) {
+        $("#class-info").html("(required)");
+        $("#class").css('background-color','#FFFFDF');
+        valid = false;
+    } 
+    if(!$("#Major".val())){
+        $("#Major-info").html("(required)");
+        $("Major").css('background-color','#FFFFDF');
+        valid = false;
+    }  
+    return valid;
+}
+</script>
+</body>
+</html>
